30582e7a79d55d6269f3e7e3efca781d03bdc06c
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-21 14:06:25 +0100

    Started developing the header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,28 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import Styles from "./Header.module.css";
+import image from "../../img/sw-logo-full.png";
 
 const Header = () => {
   return (
-    <header>
+    <header className={Styles.header}>
       <div>
-          <nav>
-              
-          </nav>
+        <nav className={Styles.navBar}>
+          <img src={image} alt="saleswhale logo" className={Styles.logo} />
+          <ul className={Styles.nav}>
+            <li className={Styles.list}>Why Saleswhale?</li>
+            <li className={Styles.list}>Product</li>
+            <li className={Styles.list}>Company</li>
+          </ul>
+          <div className={Styles.link}>
+            <a href="#" className={Styles.login}>
+              Log in
+            </a>
+            <a href="#" className={Styles.demo}>
+              Get A Demo
+            </a>
+          </div>
+        </nav>
       </div>
     </header>
   );
