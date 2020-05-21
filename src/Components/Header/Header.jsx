0235e9851ddd7319ce9230f719a678e9e3ae73aa
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-21 14:37:14 +0100

    created and styled the hero section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Styles from "./Header.module.css";
 import image from "../../img/sw-logo-full.png";
+import heroImage from "../../img/hero-img.png";
 
 const Header = () => {
   return (
@@ -24,6 +25,37 @@ const Header = () => {
           </div>
         </nav>
       </div>
+      <div className={Styles.hero}>
+        <img src={heroImage} alt="Painting" className={Styles.heroImage} />
+      </div>
+      <section className={Styles.headerText}>
+        <aside className={Styles.emphasis}>
+          <h1>A future where AI and humans works hand in hand</h1>
+        </aside>
+        <aside className={Styles.phrase}>
+          <p>
+            At Saleswhale, we believe that the future of work is Collaborative
+            Intelligence - where machines and AI help humans fulfil their
+            highest potential by automating drudgery and producing for more
+            effective results.
+          </p>
+          <p>
+            AI assistants help to automate repetitive and mundane tasks so that
+            us humans focus on creative, high-impact work that creates value and
+            inspires. The result? Happier and more productive teams doing
+            meaningful work.
+          </p>
+          <p>
+            Our mission is to put an AI assistant in every company that could
+            greatly benefit from it, and we're starting off strong addressing
+            this need in marketing and sales teams all across the world.
+          </p>
+          <p>
+            The future is where AI and humans work hand in hand, and we’re here
+            to help businesses benefit from that.
+          </p>
+        </aside>
+      </section>
     </header>
   );
 };
