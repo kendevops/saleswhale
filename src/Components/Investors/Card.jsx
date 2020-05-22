fe217dc48cc36861e4c907696cd8fb63be6651cc
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-22 15:47:22 +0100

    Added all the logos need for the card

---
+++ src/Components/Investors/Card.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Ylogo from "../../img/YC logo.png";
+import Slogo from "../../img/STRIVE_logo.png";
+import Wlogo from "../../img/wavemaker-350.png";
+import Mlogo from "../../img/MHV-logo.png";
+
+const Card = () => {
+  return;
+};
