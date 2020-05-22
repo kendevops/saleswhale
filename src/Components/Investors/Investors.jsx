2e31acd3947fcfbb6c1bb78545f6dbb094c65ae6
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-22 18:04:25 +0100

    Added the flex for the images in the card

--- src/Components/Investors/Investors.jsx
+++ src/Components/Investors/Investors.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Styles from "./Investors.module.css";
 import Card from "./Card";
 import Ylogo from "../../img/YC logo.png";
 import Slogo from "../../img/STRIVE_logo.png";
 import Wlogo from "../../img/wavemaker-350.png";
 import Mlogo from "../../img/MHV-logo.png";
+
+import Styles from "./Investors.module.css";
 
 const Investor = () => {
   return (
