f69035f18d86f3cfaf5ea7a0425c5180db1a90db
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-22 19:03:49 +0100

    Finished up design of Future component

--- src/Components/Future/Future.jsx
+++ src/Components/Future/Future.jsx
@@ -1,9 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import Styles from "./Future.module.css";
 
 const Future = () => {
   return (
-    <div>
-      <h1>Future</h1>
+    <div className={Styles.container}>
+      <div className={Styles.card}>
+        <h1>See the future</h1>
+        <div>
+          <a href="#" className={Styles.demo}>
+            Get a Demo
+          </a>
+          <a href="#" className={Styles.cases}>
+            Read use cases
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
