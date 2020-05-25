22f5eb6c4062922fa2afc87471dc067aedf2c28d
Kenneth Aladi <kennethmichael11@gmail.com>
2020-05-25 02:24:57 +0100

    Finished the design for the footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -64,7 +64,9 @@ const Footer = () => {
       </div>
       <div className={Styles.foot}>
         <div className={Styles.terms}>
-          <h3>Privacy Terms</h3>
+          <h3>
+            Privacy <span>Terms</span>
+          </h3>
           <p>© 2020 Saleswhale, Inc. All rights reserved.</p>
         </div>
         <div className={Styles.icons}>
